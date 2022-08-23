0c7d040e9c2594e8764145fa9dfc1dcd7b9d59f2
Kian Hin Tan <40939526+khtan2000@users.noreply.github.com>
2022-08-23 23:45:50 +0800

    Add files via upload

--- CO2_device/CO2_device.ino
+++ CO2_device/CO2_device.ino
@@ -12,6 +12,16 @@
 #include <curveFitting.h>
 #include <MovingAverageFilter.h>
 #include <ESP32Time.h>
+#include <WiFi.h>
+#include <esp_wifi.h>
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+#include <Wire.h>
+
+//experimental to increase stack size limit
+//#define CONFIG_ARDUINO_LOOP_STACK_SIZE 1024*32
 
 //#define DEBUG_LCD
 #define SDCARD_INIT
@@ -20,13 +30,20 @@
 #define LOW_BYTE(x)     ((byte)((x)&0xFF))
 #define HIGH_BYTE(x)    ((byte)(((x)>>8)&0xFF))
 
+//RIMS
+#define MEASURING 0
+#define NORMAL 1
+#define MILD 2
+#define SEVERE 3
+
+
 const char * app_ver = "v1.3";
 
 MovingAverageFilter movingAverageFilter(180); //Use 180 data points for moving average
 
 //GPIO pins
 const byte USER_LED1 = 5;
-const byte VBAT_ADC = 18;
+const byte VBAT_ADC = 36;
 const byte SENSOR_RXD = 22;
 const byte SENSOR_TXD = 23;
 const byte BUTTON_1 = 26;
@@ -36,6 +53,66 @@ const byte LCD_TX2 = 33; //swapped from schematics
 
 const char DELIM = ',';
 const char NEWLINE = '\n';
+
+//-------------------------------------------------------------------------------
+/////////////////////////////////// ADC /////////////////////////////////////////
+#define R2 100  //voltage divider resistor
+#define R3 100  //voltage divider resistor
+#define VOLTAGE_MAX 4200
+#define VOLTAGE_MIN 3300
+#define VOLTAGE_OUT(Vin) (((Vin) * R3) / (R2 + R3))
+#define ADC_REFERENCE 1100
+#define VOLTAGE_TO_ADC(in) ((ADC_REFERENCE * (in)) / 4096)
+#define BATTERY_MAX_ADC VOLTAGE_TO_ADC(VOLTAGE_OUT(VOLTAGE_MAX))
+#define BATTERY_MIN_ADC VOLTAGE_TO_ADC(VOLTAGE_OUT(VOLTAGE_MIN))
+
+int readADCVal = 0;
+//-------------------------------------------------------------------------------
+
+//-------------------------------------------------------------------------------
+/////////////////////////////////// Bluetooth /////////////////////////////////////////
+#define SERVICE_UUID "d351655c-f71d-49be-b432-3b6867edf8ca"
+#define bleServerName "I-Breath"
+
+// AVGCO2 Characteristic and Descriptor
+BLECharacteristic bleAVGCO2Characteristics("b4099544-4f23-4ff1-89a1-d6c5823f735e", BLECharacteristic::PROPERTY_NOTIFY);
+BLEDescriptor bleAVGCO2Descriptor(BLEUUID((uint16_t)0x2901));
+
+// ETCO2 Characteristic and Descriptor
+BLECharacteristic bleETCO2Characteristics("36043252-5dd5-457d-baf4-b6ed8267257b", BLECharacteristic::PROPERTY_NOTIFY);
+BLEDescriptor bleETCO2Descriptor(BLEUUID((uint16_t)0x2902));
+
+// RR Characteristic and Descriptor
+BLECharacteristic bleRRCharacteristics("3241d188-50d2-4325-bce3-f42987b59e0b", BLECharacteristic::PROPERTY_NOTIFY);
+BLEDescriptor bleRRDescriptor(BLEUUID((uint16_t)0x2903));
+
+// AVGACTCO2 Characteristic and Descriptor
+BLECharacteristic bleAVGACTCO2Characteristics("c90026ca-23a1-4c7a-9b5c-8eeda8dca19d", BLECharacteristic::PROPERTY_NOTIFY);
+BLEDescriptor bleAVGACTCO2Descriptor(BLEUUID((uint16_t)0x2904));
+
+// S3 Characteristic and Descriptor
+BLECharacteristic bleS3Characteristics("b38f90d9-d30d-419f-90af-7c0175690c72", BLECharacteristic::PROPERTY_NOTIFY);
+BLEDescriptor bleS3Descriptor(BLEUUID((uint16_t)0x2905));
+
+
+// name Characteristic and Descriptor
+BLECharacteristic bleNameCharacteristics("0a8f61ad-bcc3-4d95-8119-857998993ffa", BLECharacteristic::PROPERTY_NOTIFY);
+BLEDescriptor bleNameDescriptor(BLEUUID((uint16_t)0x2906));
+
+
+bool deviceConnected = false;
+
+//Setup callbacks onConnect and onDisconnect
+class MyServerCallbacks : public BLEServerCallbacks {
+  void onConnect(BLEServer *pServer) {
+    deviceConnected = true;
+  };
+  void onDisconnect(BLEServer *pServer) {
+    deviceConnected = false;
+  }
+};
+
+//-------------------------------------------------------------------------------
 
 //-------------------------------------------------------------------------------
 /////////////////////////////////// RTC /////////////////////////////////////////
@@ -51,17 +128,21 @@ const uint16_t S3_ADDR = 0x1400;
 const uint16_t CURVE_ADDR = 0x0310;
 const uint16_t RTC_SYSVAR_ADDR = 0x0010;
 const uint16_t DATA_RTN_ADDR = 0x2100;
+const uint16_t RIMS_ADDR = 0x2500;
+const uint16_t BATT_ADDR = 0x2800;
 
 const int DISP_DELAY_MS = 100;
 byte lcd_buf[100] = {};
 Ticker displayTicker;
 Ticker lcdInputTicker;
 
+
+
 //-------------------------------------------------------------------------------
 ///////////////////////////////// Buttons ///////////////////////////////////////
 const int BTN_DEBOUNCE_MS = 50;
 const int BTN_CHECK_MS = 10;
-static byte buttonPin[2] = {BUTTON_1, BUTTON_2};
+static byte buttonPin[2] = {BUTTON_2, BUTTON_1};
 static byte debouncedBtnState[2] = {1, 1};
 static bool buttonPressed[2] = {false, false};
 static byte buttonCount[2] = {BTN_DEBOUNCE_MS/BTN_CHECK_MS, BTN_DEBOUNCE_MS/BTN_CHECK_MS};
@@ -116,11 +197,25 @@ double S3deg = 0.0;
 const byte MAX_NAME_LEN = 5;
 String defaultLoggingFile = "/test1.csv";
 char currentLoggingFile[11] = {};
+File file;
 boolean sdCardAvailable = false;
 boolean loggingIsOn = false;
 String patient = "";
 char name_str[MAX_NAME_LEN + 1] = {}; //+1 for NULL
 Ticker loggingTicker;
+//----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+//////////////////////////// Asthma Classification/////////////////////////////
+int result0 = 0;
+int result1 = 0;
+int result2 = 0;
+int result3 = 0;
+int button0 = 0;  
+int button1 = 0; 
+int button2 = 0;
+int button3 = 0;
+int astma = 0;
 //----------------------------------------------------------------------------
 
 void resetData(){
@@ -132,9 +227,10 @@ void resetData(){
 }
 
 void updateCo2Disp(){
-    static float co2Avg_cached = 0.0;
-    if(co2Avg_cached != co2Avg){
-        co2Avg_cached = co2Avg;
+    //commented out as LCD requires constant refresh else value disappears
+    //static float co2Avg_cached = 0.0;
+    //if(co2Avg_cached != co2Avg){
+        //co2Avg_cached = co2Avg;
         static int16_t co2AvgPrev = 0;
         int16_t co2AvgInt = co2Avg*100;
         char buf[16] = {0x5A, 0xA5, 0x0D, 0x82, HIGH_BYTE(CURVE_ADDR), LOW_BYTE(CURVE_ADDR), 0x5A, 0xA5, 0x01, 0x00, 0x00, 0x02};
@@ -144,13 +240,14 @@ void updateCo2Disp(){
         buf[15] = (co2AvgInt & 0xFF);
         co2AvgPrev = co2AvgInt; //store current value as previous value
         Serial1.write(buf, sizeof(buf));
-    }
+    //}
 }
 
 void updateEtCo2Disp(){
-    static float etCo2_cached = -0.001;
-    if(etCo2_cached != etCo2){
-        etCo2_cached = etCo2;
+    //commented out as LCD requires constant refresh else value disappears
+    //static float etCo2_cached = -0.001;
+    //if(etCo2_cached != etCo2){
+        //etCo2_cached = etCo2;
         String str = String(etCo2);
         char buf[32] = {0x5A, 0xA5, 0x00, 0x82, HIGH_BYTE(ETCO2_ADDR), LOW_BYTE(ETCO2_ADDR)};
 
@@ -162,13 +259,14 @@ void updateEtCo2Disp(){
         else{
             Serial.println("Insufficient buffer size: ETCO2");
         }
-    }
+    //}
 }
 
 void updateRespirationRateDisp(){
-    static float RR_cached = -0.001;
-    if(RR_cached != respirationRate){
-        RR_cached = respirationRate;
+    //commented out as LCD requires constant refresh else value disappears
+    //static float RR_cached = -0.001;
+    //if(RR_cached != respirationRate){
+        //RR_cached = respirationRate;
         String str = String(respirationRate);
         char buf[32] = {0x5A, 0xA5, 0x00, 0x82, HIGH_BYTE(RR_ADDR), LOW_BYTE(RR_ADDR)};
 
@@ -180,13 +278,14 @@ void updateRespirationRateDisp(){
         else{
             Serial.println("Insufficient buffer size: Respiration Rate");
         }
-    }
+    //}
 }
 
 void updateHjorthActDisplay(){
-    static float HA_cached = 0.0;
-    if(HA_cached != hjorthActivity){
-        HA_cached = hjorthActivity;
+    //commented out as LCD requires constant refresh else value disappears
+    //static float HA_cached = 0.0;
+    //if(HA_cached != hjorthActivity){
+        //HA_cached = hjorthActivity;
         String str = String(hjorthActivity);
         char buf[32] = {0x5A, 0xA5, 0x00, 0x82, HIGH_BYTE(HA_ADDR), LOW_BYTE(HA_ADDR)};
 
@@ -198,13 +297,14 @@ void updateHjorthActDisplay(){
         else{
             Serial.println("Insufficient buffer size: Hjorth Activity");
         }
-    }
+    //}
 }
 
 void updateS3DegDisplay(){
-    static float S3deg_cached = 0.0;
-    if(S3deg_cached != S3deg){
-        S3deg_cached = S3deg;
+    //commented out as LCD requires constant refresh else value disappears
+    //static float S3deg_cached = 0.0;
+    //if(S3deg_cached != S3deg){
+        //S3deg_cached = S3deg;
         String str = String(S3deg);
         char buf[32] = {0x5A, 0xA5, 0x00, 0x82, HIGH_BYTE(S3_ADDR), LOW_BYTE(S3_ADDR)};
 
@@ -216,12 +316,135 @@ void updateS3DegDisplay(){
         else{
             Serial.println("Insufficient buffer size: S3(degree)");
         }
+    //}
+}
+
+void updateRIMSDisplay(int status){
+
+    
+    if(NORMAL == status)
+    {
+       //No Anomaly
+       byte buf[]={0x5A,0xA5,0x0D,0x82,0x25,0x00,0x4E,0x6F,0x20,0x41,0x6E,0x6F,0x6D,0x61,0x6C,0x79};
+       Serial1.write(buf,sizeof(buf));
     }
+    else if(MILD == status)
+    {
+       //Mild
+       byte buf[]={0x5A,0xA5,0x0D,0x82,0x25,0x00,0x4D,0x69,0x6C,0x64,0x00,0x00,0x00,0x00,0x00,0x00};
+       Serial1.write(buf,sizeof(buf));
+    }
+    else if(SEVERE == status)
+    {
+       //Severe
+       byte buf[]={0x5A,0xA5,0x0D,0x82,0x25,0x00,0x53,0x65,0x76,0x65,0x72,0x65,0x00,0x00,0x00,0x00};
+       Serial1.write(buf,sizeof(buf));
+    }
+    else
+    {
+       //Measuring
+       byte buf[]={0x5A,0xA5,0x0D,0x82,0x25,0x00,0x4D,0x65,0x61,0x73,0x75,0x72,0x69,0x6E,0x67,0x00};
+       Serial1.write(buf,sizeof(buf));
+    }
+    
+}
+
+void startBLEService()
+{
+
+  Serial.print("ESP Board MAC Address:  ");
+  Serial.println(WiFi.macAddress());
+  //turn off BT classic to save memory
+  esp_bt_controller_mem_release(ESP_BT_MODE_CLASSIC_BT);
+  
+  // Create the BLE Device
+  BLEDevice::init(bleServerName);
+
+  // Create the BLE Server
+  BLEServer *pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Create the BLE Service
+  BLEService *bmeService = pServer->createService(SERVICE_UUID);
+
+  // Create BLE Characteristics and Create a BLE Descriptor
+  bmeService->addCharacteristic(&bleAVGCO2Characteristics);
+  bleAVGCO2Descriptor.setValue("AVGCO2");
+  bleAVGCO2Characteristics.addDescriptor(&bleAVGCO2Descriptor);
+
+  // Create BLE Characteristics and Create a BLE Descriptor
+  bmeService->addCharacteristic(&bleETCO2Characteristics);
+  bleETCO2Descriptor.setValue("ETCO2");
+  bleETCO2Characteristics.addDescriptor(&bleETCO2Descriptor);
+
+  // Create BLE Characteristics and Create a BLE Descriptor
+  bmeService->addCharacteristic(&bleRRCharacteristics);
+  bleRRDescriptor.setValue("RR");
+  bleRRCharacteristics.addDescriptor(&bleRRDescriptor);
+
+    // Create BLE Characteristics and Create a BLE Descriptor
+  bmeService->addCharacteristic(&bleAVGACTCO2Characteristics);
+  bleAVGACTCO2Descriptor.setValue("AVGACTCO2");
+  bleAVGACTCO2Characteristics.addDescriptor(&bleAVGACTCO2Descriptor);
+
+    // Create BLE Characteristics and Create a BLE Descriptor
+  bmeService->addCharacteristic(&bleS3Characteristics);
+  bleS3Descriptor.setValue("S3");
+  bleS3Characteristics.addDescriptor(&bleS3Descriptor);
+
+      // Create BLE Characteristics and Create a BLE Descriptor
+  bmeService->addCharacteristic(&bleNameCharacteristics);
+  bleNameDescriptor.setValue("Patient Name");
+  bleNameCharacteristics.addDescriptor(&bleNameDescriptor);
+
+  // Start the service
+  bmeService->start();
+
+  // Start advertising
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pServer->getAdvertising()->start();
+
+  Serial.println("Waiting a client connection to notify...");
+}
+
+
+
+void updateBLEData(){
+      char dataConvert[6];
+      
+      //Set Characteristic value and notify connected client
+      dtostrf(co2Avg, 5, 2, dataConvert);
+      bleAVGCO2Characteristics.setValue(dataConvert);
+      bleAVGCO2Characteristics.notify();
+
+      dtostrf(etCo2, 5, 2, dataConvert);
+      bleETCO2Characteristics.setValue(dataConvert);
+      bleETCO2Characteristics.notify();
+
+      dtostrf(respirationRate, 5, 2, dataConvert);
+      bleRRCharacteristics.setValue(dataConvert);
+      bleRRCharacteristics.notify();
+
+      dtostrf(hjorthActivity, 5, 2, dataConvert);
+      bleAVGACTCO2Characteristics.setValue(dataConvert);
+      bleAVGACTCO2Characteristics.notify();
+
+      dtostrf(S3deg, 5, 2, dataConvert);
+      bleS3Characteristics.setValue(dataConvert);
+      bleS3Characteristics.notify();
+      
+      patient.toCharArray(dataConvert, 5);
+      bleNameCharacteristics.setValue(dataConvert);
+      bleNameCharacteristics.notify();
 }
 
 void updateDisplay(){
     if(loggingIsOn){
         updateDisplayForce();
+        if (deviceConnected){
+             updateBLEData();
+        }
     }
 }
 
@@ -239,6 +462,31 @@ void requestCurrTime(){
         Serial1.flush(); //flush outgoing data if any - to ensure data reply is clean
         Serial1.write(buf, sizeof(buf));
     }
+}
+
+void calc_battery_percentage(){
+    readADCVal = analogRead(VBAT_ADC);
+    int battery_percentage = 100 * (readADCVal - BATTERY_MIN_ADC) / (BATTERY_MAX_ADC - BATTERY_MIN_ADC);
+
+    if (battery_percentage < 0){
+        battery_percentage = 0;
+    }
+    else if (battery_percentage > 100){
+        battery_percentage = 100;
+    }
+
+    String str = String(battery_percentage);
+    char buf[32] = {0x5A, 0xA5, 0x00, 0x82, HIGH_BYTE(BATT_ADDR), LOW_BYTE(BATT_ADDR)};
+
+    if((str.length() + 6) <= sizeof(buf)){
+         buf[2] = str.length() + 3; //update length
+         memcpy(&buf[6], str.c_str(), str.length());
+         Serial1.write(buf, (str.length() + 6));
+     }
+     else{
+         Serial.println("Insufficient buffer size: BATT");
+     }
+    
 }
 
 void isLoggingOn(){
@@ -271,7 +519,7 @@ void isLoggingOn(){
 
             Serial.print(header.c_str());
             if(sdCardAvailable){
-                appendFile(currentLoggingFile, const_cast<char *>(header.c_str()));
+                openFile(currentLoggingFile, const_cast<char *>(header.c_str()));
             }
             
             digitalWrite(USER_LED1, HIGH);
@@ -285,6 +533,7 @@ void isLoggingOn(){
             resetData();
             updateDisplayForce();
             digitalWrite(USER_LED1, LOW);
+            closeFile();
         }
     }
 }
@@ -312,6 +561,28 @@ void deleteFileIfAvailable(const char * path){
                 Serial.println("Delete failed");
             } 
         }
+    }
+}
+
+void openFile(const char * path, const char * data){
+    file = SD_MMC.open(path, FILE_APPEND);
+    if(!file){
+        Serial.println("Failed to open file for writing");
+        return;
+    }
+    if(!file.println(data)){
+        Serial.println("Write failed");
+    }
+    //file.close();
+}
+
+void closeFile(){
+    file.close();
+}
+
+void writeFile(const char * path, const char * data){
+    if(!file.println(data)){
+          Serial.println("Append failed");
     }
 }
 
@@ -616,7 +887,7 @@ void doLogging(){
         Serial.print(log.c_str());
 
         if(sdCardAvailable){
-            appendFile(currentLoggingFile, const_cast<char *>(log.c_str()));
+            openFile(currentLoggingFile, const_cast<char *>(log.c_str()));
         }
     }
 }
@@ -636,7 +907,7 @@ void setup() {
 
     Serial.println("... Initializing CO2 sensor.");
     co2Sensor.init();
-
+      
     memcpy(currentLoggingFile, defaultLoggingFile.c_str(), defaultLoggingFile.length());
 
     #ifdef SDCARD_INIT
@@ -658,11 +929,92 @@ void setup() {
     displayTicker.attach_ms(DISP_DELAY_MS, updateDisplay);
     lcdInputTicker.attach_ms(DISP_DELAY_MS, checkInputCmd);
 
+    //start BLE service
+    startBLEService();
+    
     Serial.println("... Initialization completed. Push button to start logging.");
     requestCurrTime(); //query RTC from LCD module
+    
+}
+
+void updateAsthmaSeverity(){
+  //Turns Count algorithm for classification of asthma severity
+  if ((etCo2 >= 32) && (etCo2 <= 45) && (respirationRate > 10) && (respirationRate < 30)){
+    result1 = result1 + 1;
+    result0 = 0;
+    result2 = 0;
+    result3 = 0;
+  }
+
+  if ((button1 == 1) && (etCo2 == 0) && (respirationRate == 0)){
+    button1 = 0;
+  }
+
+  if ((result1 == 20) && (button1 == 0)){
+    updateRIMSDisplay(NORMAL); 
+    astma = 1;
+    result1 = 0;
+    button1 = 1;
+  }
+
+
+  if ((etCo2 < 32) && (respirationRate > 28)){
+    result2 = result2 + 1;
+    result0 = 0;
+    result1 = 0;
+    result3 = 0;
+  }
+
+  if ((button2 == 1) && (etCo2 == 0) && (respirationRate == 0)){
+    button2 = 0;
+  }
+
+  if ((result2 == 25) && (button2 == 0)){
+    updateRIMSDisplay(MILD); 
+    astma = 5;
+    result2 = 0;
+    button2 = 1;
+  }
+  
+  if ((etCo2 >= 45 ) && (respirationRate < 8)){
+    result0 = result0 + 1;
+    result1 = 0;
+    result2 = 0;
+    result3 = 0;
+  }
+
+  if ((button0 == 1) && (etCo2 == 0) && (respirationRate == 0)){
+    button0 = 0;
+  }
+
+  if ((result0 == 8) && (button0 == 0)){
+    updateRIMSDisplay(SEVERE);
+    astma = 10;
+    result0 = 0;
+    button0 = 1;
+  }
+
+  if ((etCo2 <= 5 ) ||  (respirationRate <= 5)){
+    result3 = result3 + 1;
+    result0 = 0;
+    result1 = 0;
+    result2 = 0;
+  }
+
+  if ((button3 == 1) && (etCo2 == 0) && (respirationRate == 0)){
+    button3 = 0;
+  }
+
+  if ((result3 == 4) && (button3 == 0)){
+    updateRIMSDisplay(MEASURING);
+    astma = 0;
+    result3 = 0;
+    button3 = 1;
+  }
 }
 
 void loop() {
+    calc_battery_percentage();
     isLoggingOn();
     co2Sensor.read();
     while(co2Sensor.isAvailable()){
@@ -673,6 +1025,7 @@ void loop() {
             updateRespirationRate(co2Packet);
             updateInspiration(co2Packet);
             doLogging();
+            updateAsthmaSeverity();
         }
     }
 }
